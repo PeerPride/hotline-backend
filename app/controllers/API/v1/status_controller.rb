e8a0479b6e6ce72e5b1785651ef04f0989ca1d05
Chris Garaffa <chris@chrisgaraffa.com>
2021-12-13 18:32:46 -0500

    Create an API endpoint for system status

---
+++ app/controllers/API/v1/status_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    class StatusController < API::BaseController # :nodoc:
+      def index
+        systemStatus = []
+
+        systemStatus[:telephonyClient] = !@telephonyClient.nil?
+        #TODO: If false, then dig into errors
+
+        render json: systemStatus
+      end
+
+    end
+  end
+end
