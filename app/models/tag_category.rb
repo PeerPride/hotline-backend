07ad11ad12f05fd662e15183458a6f505853ed5d
Chris Garaffa <chris@chrisgaraffa.com>
2022-02-05 13:44:57 -0500

    Tag/Category

--- app/models/tag_category.rb
+++ app/models/tag_category.rb
@@ -1,3 +1,5 @@
 class TagCategory < ApplicationRecord
   include Orderable
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- test/models/tag_category_test.rb
+++ test/models/tag_category_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class TagCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Tag Category requires a unique name" do
+    tc1 = TagCategory.new
+    tc1.name = 'Category'
+    tc1.save!
+
+    tc2 = TagCategory.new
+    tc2.name = 'Category'
+    assert_raises(ActiveRecord::RecordInvalid) do
+      cp2.save!
+    end
+  end
+
+  test "Tag Category requires a name" do
+    tc1 = TagCategory.new
+    assert_raises(ActiveRecord::RecordInvalid) do
+      tc1.save!
+    end
+  end
 end
