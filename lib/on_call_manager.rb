6a666476428fc2f5d2dee6e51b34be6b29227976
Chris Garaffa <chris@chrisgaraffa.com>
2022-07-02 14:36:54 -0400

    Fix put_on_call / take_off_call for cache

--- lib/on_call_manager.rb
+++ lib/on_call_manager.rb
@@ -5,21 +5,23 @@ class OnCallManager
   ON_CALL_KEY_BASE = 'on_call'
 
   def self.put_on_call(user, method, language)
-    key_name = "#{ON_CALL_KEY_BASE}_#{method}_#{language.name}"
+    key_name = "#{ON_CALL_KEY_BASE}_#{language.name.downcase}"
+    puts key_name
 
-    key_values = JSON.parse(Rails.cache.read(key_name))
+    key_values = Rails.cache.read(key_name)
+    puts key_values
 
     if key_values.nil?
-      Redis.cache.write(key_name, [user.id])
-    elsif !x.include? user.id
-      Redis.cache.write(key_name, key_values.push(user.id))
+      Rails.cache.write(key_name, [user.id])
+    elsif !key_values.include? user.id
+      Rails.cache.write(key_name, key_values.push(user.id))
     end
   end
 
   def self.take_off_call(user, method, language)
-    key_name = "#{ON_CALL_KEY_BASE}_#{method}_#{language.name}"
+    key_name = "#{ON_CALL_KEY_BASE}_#{language.name}"
 
-    key_values = JSON.parse(Rails.cache.read(key_name))
+    key_values = Rails.cache.read(key_name)
 
     if key_values.nil?
       nil
